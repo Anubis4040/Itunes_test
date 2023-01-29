008fceea2e6db0892bc47755c6ba024a4c183958
Jose Gonzalez <joseignacio4040@gmail.com>
2023-01-29 18:02:09 -0400

    Set songs as favorite

--- src/index.js
+++ src/index.js
@@ -1,53 +1,73 @@
-require('dotenv').config();
-const express = require('express')
-const morgan = require('morgan')
-const http = require('./http-common')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const http = require("./httpCommon");
+const fsManager = require("./fsManager");
 
-app = express()
-const router = express.Router();
+app = express();
 
-app.use(morgan('dev'))
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+const favorites = [];
 
 //List song by artist
-app.get('/songs/', async (req, res) => {
+app.get("/songs", async (req, res) => {
+  const query = req.query;
 
-    const query = req.query
+  const params = {
+    media: "music",
+    entity: "song",
+    attribute: "artistTerm",
+    term: query.artistName,
+    limit: 25,
+  };
 
-    const params = {
-        media:'music',
-        entity:'song',
-        attribute:'artistTerm',
-        term:query.artistName,
-        limit:25
-    }
+  const resultSet = await http.get(`${process.env.ITUNES_API_URL}/search`, {
+    params,
+  });
 
-    const resultSet = await http.get(
-        process.env.ITUNES_API_URL,{
-            params
-        }
-    )
+  // console.log(resultSet.data, 'resultSet')
 
-    // console.log(resultSet.data, 'resultSet')
+  const albums = Array.from(
+    new Set(resultSet.data.results.map((item) => item.collectionName))
+  );
 
-    const albums = Array.from(new Set(resultSet.data.results.map((item) => item.collectionName)))
+  console.log(albums, "albums");
 
-    console.log(albums, 'albums')
-
-    res.json({
-        resultsCount: resultSet.data.resultCount,
-        almbumsCount: albums.length,
-        albums: albums,
-        results: resultSet.data.results
-    })
-})
+  res.json({
+    resultsCount: resultSet.data.resultCount,
+    almbumsCount: albums.length,
+    albums: albums,
+    results: resultSet.data.results,
+  });
+});
 
 //Set song as favorite
+app.post("/favorites", async (req, res, next) => {
+  const body = req.body;
 
+  const params = {
+    id: body.trackId,
+  };
 
+  const resultSet = await http.get(`${process.env.ITUNES_API_URL}/lookup`, {
+    params,
+  });
+
+  if (resultSet.data.resultCount > 0) {
+    favorites.push(resultSet.data.results[0]);
+    res.json({
+      message: "Song successfully set as favorite",
+    });
+  } else {
+    res.status(404).json({
+      message: "Song not found",
+    });
+  }
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server runing on port ${process.env.PORT}`)
-})
+  console.log(`Server runing on port ${process.env.PORT}`);
+});
